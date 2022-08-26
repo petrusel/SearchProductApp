8f9ae26f14ddba511ad3b8a3ef641ab0ac5b2cd3
petrusel <petruselgheorghenicusor@gmail.com>
2022-08-26 20:07:17 +0300

    add users and roles

---
+++ src/main/java/petrusel/myapp/v1/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package petrusel.myapp.v1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import petrusel.myapp.v1.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getUserByUsername(String username);
+}
---
+++ src/main/java/petrusel/myapp/v1/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package petrusel.myapp.v1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import petrusel.myapp.v1.model.User;
+import petrusel.myapp.v1.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new MyUserDetails(user);
+    }
+}
